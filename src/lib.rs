9160e21afc055a4b6cbb885556359d0b34a7a6ea
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-02 10:21:16 -0700

    Add bindings for detectors

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,38 @@
 
 #![deny(clippy::all)]
 
-pub(crate) mod class;
-pub(crate) mod interface;
-pub(crate) mod module;
+use js_sys::Promise;
+use wasm_bindgen::prelude::*;
 
-pub use class::*;
-pub use interface::*;
-pub use module::*;
+#[wasm_bindgen(module = "wasm-feature-detect")]
+extern {
+    #[wasm_bindgen(js_name = "bigInt")]
+    pub fn big_int() -> Promise;
+
+    #[wasm_bindgen(js_name = "bulkMemory")]
+    pub fn bulk_memory() -> Promise;
+
+    pub fn exceptions() -> Promise;
+
+    #[wasm_bindgen(js_name = "multiValue")]
+    pub fn multi_value() -> Promise;
+
+    #[wasm_bindgen(js_name = "mutableGlobals")]
+    pub fn mutable_globals() -> Promise;
+
+    #[wasm_bindgen(js_name = "referenceTypes")]
+    pub fn reference_types() -> Promise;
+
+    #[wasm_bindgen(js_name = "saturatedFloatToInt")]
+    pub fn saturated_float_to_int() -> Promise;
+
+    #[wasm_bindgen(js_name = "signExtensions")]
+    pub fn sign_extensions() -> Promise;
+
+    pub fn simd() -> Promise;
+
+    #[wasm_bindgen(js_name = "tailCall")]
+    pub fn tail_call() -> Promise;
+
+    pub fn threads() -> Promise;
+}
